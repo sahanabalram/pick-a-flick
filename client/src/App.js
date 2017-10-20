619481591c354ac2ae26f3a614bb067a58e70654
Sahana Balram <sahana2008balram@gmail.com>
2017-10-20 14:46:15 -0700

    intial setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import SideNavbar from './components/SideNav';
+
+
+class App extends Component {
+  render() {
+    return(
+      <SideNavbar />
+    )
+  }
+}
+
+export default App;
