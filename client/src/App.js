493bfed04a19f1bd5ee276d6c4c6ff9877dd4c82
annacrespo12 <annacrespo12@gmail.com>
2017-10-23 10:58:53 -0700

    materialize card

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import SideNavbar from './components/SideNav';
-
+import CardExampleExpandable from './components/Cards';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
   render() {
     return(
-      <SideNavbar />
+      <div>
+         <SideNavbar />
+        <MuiThemeProvider>
+          <CardExampleExpandable />
+        </MuiThemeProvider>
+        
+     </div>
     )
   }
 }
