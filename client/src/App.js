f2f15236c2aba318c7eec55bc785d0e66ad72685
Jeanelle <jeanellesebastian@gmail.com>
2017-10-23 11:18:17 -0700

    Added my name suckas

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,10 @@ import SideNavbar from './components/SideNav';
 class App extends Component {
   render() {
     return(
+      <div>
+      <h1>Jeanelllleeeee</h1>
       <SideNavbar />
+      </div>
     )
   }
 }
