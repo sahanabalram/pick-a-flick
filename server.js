a7cc9999f2a0f2b797325e4eb0825665d60088ac
Sahana Balram <sahana2008balram@gmail.com>
2017-10-26 14:56:42 -0700

    Readd server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+const logger = require("morgan");
+
+// grabbing our movie model
+const db = require("./models");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// logging for request to the console
+app.use(logger("dev"));
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static("client/public"));
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+  if ( process.env.NODE_ENV === "production" ) {
+    res.sendFile(__dirname + "./client/build/index.html");
+  } else {
+    res.sendFile(__dirname + "./client/public/index.html");
+  }
+});
+
+db.sequelize.sync().then(function() {
+  app.listen(PORT);
+});
