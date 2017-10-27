21e9182e9e9f956fb04c8a5a1884ee1f041ab74c
Sahana Balram <sahana2008balram@gmail.com>
2017-10-26 19:08:43 -0700

    Chatbot working

--- client/src/components/ChatBot/ChatBot.js
+++ client/src/components/ChatBot/ChatBot.js
@@ -3,10 +3,10 @@ import "./ChatBot.css"
 class ChatbotContainer extends Component {
     render() {
         return (
-            <iframe id="chatterbot" title="chattybot"
+            <iframe
             width="350"
             height="430"
-            src="https://console.dialogflow.com/api-client/demo/embedded/13205648-e9a8-4cb9-b8f8-a73fceca689e">
+            src="https://console.dialogflow.com/api-client/demo/embedded/174e0285-cc65-4d9b-87ab-b6b7a9ad4426">
         </iframe>
         )
     }
